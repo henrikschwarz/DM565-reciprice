6c424b3299a3cd897af24bb67fc2a865d212a433
Lucas Olai Jarlkov Olsen <lucas-olai@hotmail.com>
2019-12-15 19:26:24 +0100

    Made 'persons' query optional when scraping

--- prototype/scrape_recipe_ingredients.py
+++ prototype/scrape_recipe_ingredients.py
@@ -5,25 +5,31 @@ import urllib.request
 def clean_soup(mess):
     return mess.get_text().strip().lower()
 
-def get_recipe_ingredients(recipe_id, persons=4):
+
+def get_recipe_ingredients(recipe_id, persons=None):
     # recipe_base_url = 'https://www.dk-kogebogen.dk/opskrifter/'
-    ingredient_base_url = 'https://www.dk-kogebogen.dk/opskrifter/udskrift/u-kommentar-u-beregn-u-billede.php?id=%s&personer=%s'
-    url = ingredient_base_url % (recipe_id, persons)
+    ingredient_base_url = 'https://www.dk-kogebogen.dk/opskrifter/udskrift/u-kommentar-u-beregn-u-billede.php?id=%s'
+    if persons is not None:
+        ingredient_base_url += '&personer=%s'
+        url = ingredient_base_url % (recipe_id, persons)
+    else:
+        url = ingredient_base_url % recipe_id
     html = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(html, 'html.parser')
 
     return [get_ingredients(soup), get_recipe(soup)]
-        
+
 
 def get_recipe(soup):
-    recipetable = soup.findAll('td', {'width': 300})[4] #find the table containing the recipe
+    recipetable = soup.findAll('td', {'width': 300})[4]  # find the table containing the recipe
     for head in recipetable.findAll('table'):
         head.extract()
     recipe = clean_soup(recipetable)
     return recipe
 
+
 def get_ingredients(soup):
-    table = soup.find('table', {'cellpadding': 3}) #find the table containing ingredients
+    table = soup.find('table', {'cellpadding': 3})  # find the table containing ingredients
     rows = table.findAll('tr')
     ingredients = []
     for row in rows:
@@ -35,5 +41,6 @@ def get_ingredients(soup):
         if len(a_t) + len(u_t) + len(i_t) > 0:
             ingredients.append([a_t, u_t, i_t])
     return ingredients
+
 
 print(get_recipe_ingredients(33005, 4))
