40d61b4dfef4e79a8b17ca27ec4ae8a236395271
Peter Heilbo Ratgen <peterratgen@gmail.com>
2019-12-15 19:57:59 +0100

    reciepe add

--- prototype/reciprice/main.py
+++ prototype/reciprice/main.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 
 from .extentions import mongo
 
+from .models import Recipe
 from datetime import datetime, timezone
 
 main = Blueprint("main", __name__)
@@ -27,3 +28,9 @@ def user_create(username):
     users = mongo.db.users
     users.insert({'_id': username, 'created': datetime.now(timezone.utc)})
     return '<h1>Created %s!</h1>' % str(username)
+
+@main.route("/recipecreate/<name>")
+def recipecreate_create(name):
+    re = Recipe(name, 'prode', ['fdd','fdds'], 'dk-kogebogen.dk', datetime.utcnow())
+    re.insert()
+    return '<h1>Created %s!</h1>' % 'lol' 
--- prototype/reciprice/models.py
+++ prototype/reciprice/models.py
@@ -2,13 +2,10 @@ from datetime import datetime, timezone
 
 from .extentions import mongo
 
-db = mongo.db
-users = db.users
-
 
 class User:
     def __init__(self, username, created):
-        self.username = username
+        self._id = username
         self.created = created
 
     # Returns the dictionary key that identifies this user
@@ -17,28 +14,31 @@ class User:
 
     # Returns the dictionary representation of this user
     def get_dict(self):
-        return {'_id': self.username, 'created': self.created}
+        return self.__dict__ 
 
     # Replaces the database user date with data from this object.
     # Will create the user, if it does not already exist.
     def replace(self):
+        users = mongo.db.users
         users.replace_one(self.get_id(), self.get_dict(), upsert=True)
 
     # Changes the username, and optionally updates the database as well
     def set_username(self, username, update_db=True):
+        users = mongo.db.users
         if update_db:
             users.update_one(self.get_id(), {'_id': username})
         self.username = username
 
     # Changes the creation time, and optionally updates the database as well
     def set_created(self, created, update_db=True):
+        users = mongo.db.users
         if update_db:
             users.update_one(self.get_id(), {'created': created})
         self.created = created
 
-
 # Returns a user object with data populated from the database
 def load_user(username):
+    users = mongo.db.users
     user = users.find_one({'_id': username})
     return User(user['_id'], user['created'])
 
@@ -50,4 +50,24 @@ def create_user(username):
     user.replace()
     return user
 
+class Recipe:
+    def __init__(self, name, procedure, ingredient_list, source, created):
+        self._id = name
+        self.procedure = procedure
+        self.ingredient_list = ingredient_list
+        self.source = source
+        self.created = created
+
+    def insert(self):
+        recipes = mongo.db.recipes
+        recipes.insert(self.__dict__)
+ 
+
+class Ingredient:
+    def __init__(self, name, alias, created, price_estimate, price_history):
+        self.name = name
+        self.alias = alias
+        self.created = created
+        self.price_estimate = price_estimate
+        self.price_history = price_history
 
