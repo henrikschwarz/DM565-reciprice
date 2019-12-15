cd0e0fd1404670f9757540fd9949f52c60be9c9f
Lucas Olai Jarlkov Olsen <lucas-olai@hotmail.com>
2019-12-15 05:34:27 +0100

    Cleared up db

--- prototype/reciprice/main.py
+++ prototype/reciprice/main.py
@@ -2,11 +2,26 @@ from flask import Blueprint, render_template
 
 from .extentions import mongo
 
+from datetime import datetime, timezone
+
 main = Blueprint("main", __name__)
 
 
 @main.route('/')
 def index():
-    user_collection = mongo.db.users
-    user_collection.insert({'name': 'Lucas'})
-    return '<h1>Added Lucas!</h1>'
+    users = mongo.db.users
+    return '<h1>Users in the system: %s</h1>' % str(users.count_documents({}))
+
+
+@main.route("/user/<username>")
+def user_profile(username):
+    users = mongo.db.users
+    user = users.find_one_or_404({"_id": username})
+    return '<h1>%s</h1>' % str(user)
+
+
+@main.route("/usercreate/<username>")
+def user_create(username):
+    users = mongo.db.users
+    users.insert({'_id': username, 'created': datetime.now(timezone.utc)})
+    return '<h1>Created %s!</h1>' % str(username)
