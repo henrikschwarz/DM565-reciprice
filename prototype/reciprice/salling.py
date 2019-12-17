c3d658c87f4eeb36164aeaefb67d5aa88dc7c37c
Lucas Olai Jarlkov Olsen <lucas-olai@hotmail.com>
2019-12-17 17:19:39 +0100

    Fixed encoding

--- prototype/reciprice/salling.py
+++ prototype/reciprice/salling.py
@@ -6,11 +6,13 @@ PRODUCT_SUGGESTIONS = 'product-suggestions/relevant-products?query=%s'
 
 
 def query_product_suggestions(query):
-    print(query + ':')
     headers = {"Authorization": "Bearer eb20ca15-7deb-4752-be07-bcb1abeb467a"}
-    response = requests.get((BASE_URL + PRODUCT_SUGGESTIONS) % query, data=None, headers=headers).json()
-    return response
+    response = requests.get((BASE_URL + PRODUCT_SUGGESTIONS) % query, data=None, headers=headers)
+    js = response.json()
+    return js
 
 
 def pretty_print(data):
-    print(json.dumps(data, indent=4))
+    print(json.dumps(data, indent=4, ensure_ascii=False))
+
+pretty_print(query_product_suggestions('mælk'))
